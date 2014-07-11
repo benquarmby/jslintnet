503d64211a6055adb5c5b467311479b1ebe5b3d4
Ben Quarmby <ben@quarm.by>
2014-07-12 07:53:15 +1000

    Version 1.6.3.

--- source/JSLintNet/Properties/AssemblyInfo.cs
+++ source/JSLintNet/Properties/AssemblyInfo.cs
@@ -35,7 +35,7 @@ namespace JSLintNet
         /// <summary>
         /// The full version number for JSLint.NET.
         /// </summary>
-        public const string Version = "1.6.2";
+        public const string Version = "1.6.3";
 
         /// <summary>
         /// The copyright line for JSLint.NET.
