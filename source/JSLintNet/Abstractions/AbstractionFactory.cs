b401087be3d9ce39e70d234c99bb5f069d631fe0
Ben Quarmby <ben@quarm.by>
2014-12-18 20:57:02 +1100

    Removed redundant dynamic assembly resolver.

--- source/JSLintNet/Abstractions/AbstractionFactory.cs
+++ source/JSLintNet/Abstractions/AbstractionFactory.cs
@@ -1,21 +1,9 @@
-﻿#pragma warning disable 1591
-
-namespace JSLintNet.Abstractions
+﻿namespace JSLintNet.Abstractions
 {
-    using System;
-    using System.IO;
-    using System.Reflection;
     using Microsoft.Build.Framework;
 
     internal class AbstractionFactory : IAbstractionFactory
     {
-        private const string JavaScriptLibrary = "Noesis.Javascript";
-
-        static AbstractionFactory()
-        {
-            AppDomain.CurrentDomain.AssemblyResolve += OnDomainAssemblyResolve;
-        }
-
         public ITaskLoggingHelper CreateTaskLoggingHelper(ITask task)
         {
             return new TaskLoggingHelper(task);
@@ -29,27 +17,6 @@ namespace JSLintNet.Abstractions
         public IJavaScriptContext CreateJavaScriptContext()
         {
             return new JavaScriptContext();
-        }
-
-        private static Assembly OnDomainAssemblyResolve(object sender, ResolveEventArgs e)
-        {
-            if (e.Name.StartsWith(JavaScriptLibrary))
-            {
-                return LoadJavaScriptAssembly();
-            }
-
-            return null;
-        }
-
-        private static Assembly LoadJavaScriptAssembly()
-        {
-            var jsLintPath = new Uri(typeof(AbstractionFactory).Assembly.CodeBase).LocalPath;
-            var libraryPath = Path.Combine(
-                Path.GetDirectoryName(jsLintPath),
-                Environment.Is64BitProcess ? "amd64" : "x86",
-                JavaScriptLibrary + ".dll");
-
-            return Assembly.LoadFile(libraryPath);
         }
     }
 }
