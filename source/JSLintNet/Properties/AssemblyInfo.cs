cf808bea17a39f5b7386905a7fab78144953cfe7
Ben Quarmby <ben@quarm.by>
2016-04-23 09:01:33 +1000

    JSLint 2016-04-08.

--- source/JSLintNet/Properties/AssemblyInfo.cs
+++ source/JSLintNet/Properties/AssemblyInfo.cs
@@ -60,7 +60,7 @@ namespace JSLintNet
         /// <summary>
         /// The edition of JSLint bundled with this release.
         /// </summary>
-        public const string Edition = "2016-03-03";
+        public const string Edition = "2016-04-08";
 
         /// <summary>
         /// The public key for JSLint.NET.
