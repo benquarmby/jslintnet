4aeb26bbafc0f9ee66df536543d03b73cfd98850
Ben Quarmby <ben@quarm.by>
2016-08-07 12:24:06 +1000

    Generated options control.

---
+++ source/JSLintNet/UI/Views/OptionsControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace JSLintNet.UI.Views
+{
+    using System.Windows.Controls;
+
+    public partial class OptionsControl : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OptionsControl"/> class.
+        /// </summary>
+        public OptionsControl()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
